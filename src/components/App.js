1797de9a146f03b254cc5207f1ebcfc68221ade7
Robert Tassy <Rtass001@fiu.edu>
2016-05-26 15:32:43 -0400

    setting up app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,38 @@
-require('normalize.css/normalize.css');
-require('styles/App.css');
+import 'normalize.css/normalize.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'styles/App.css';
 
-import React from 'react';
+import React, { Component } from 'react';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import { Grid, Row, Col } from 'react-boostrap';
 
 let yeomanImage = require('../images/yeoman.png');
 
-class AppComponent extends React.Component {
+class AppComponent extends Component {
+
+  componentWillMount() {
+    this.lock = new Auth0Lock('AUTH_ID', 'Auth_Domain');
+  }
+
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      <div>
+        <Header lock={this.lock}></Header>
+        <Grid>
+          <Row>
+            <Col xs={12} md={3}>
+              <Sidebar></Sidebar>
+            </Col>
+            <Col xs={12} md={9}>
+            {this.props.children}
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
 }
 
-AppComponent.defaultProps = {
-};
 
 export default AppComponent;
